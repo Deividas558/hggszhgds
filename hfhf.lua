942bcd005c67ede3cfed1c75bb6afaabb276de3b
Deividas558 <deividasggbboonn@gmail.com>
2025-02-19 16:48:26 +0200

    Create hfhf.lua

---
+++ hfhf.lua
@@ -0,0 +1,5 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local window = Library.Createlib("nooobbgo"), "RJTheme3")
+
+local Tab = Window:NewTab("teleport")
